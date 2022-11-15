97d2f01adbd55a4759ade840ad4edd52747b131f
Mmanzi <mmanzicder@gmail.com>
2022-11-15 21:39:33 +0200

    pr init

---
+++ src/main/java/com/dev/tover/ToverApplication.java
@@ -0,0 +1,13 @@
+package com.dev.tover;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToverApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dev/tover/ToverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.tover;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
